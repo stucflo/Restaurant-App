6064ca71dfdd975898a9bee09cf614db404c72b9
Stuart Campbell <stuartcampbell@Stuarts-iMac-2.local>
2018-09-26 22:35:50 -0500

    Beginning HTML

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic3R1MzEyIiwiYSI6ImNqbWp6MDlpZjBkdTMzcnFzNmh2OTByeXoifQ.XuO-W0GOL3dnYW8kjpoa4w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
